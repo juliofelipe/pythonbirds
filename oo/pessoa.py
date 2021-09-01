c42ff529ad0cecc2e054e1a43f5e0dfd7d0531b6
Julio Felipe <julio.felipe@casaldev.com.br>
2021-09-01 10:19:36 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,14 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=43):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá'
+
+
+if __name__=='__main__':
+    p = Pessoa()
+    print(p.cumprimentar())
+    p.nome = 'Julio'
+    print(p.nome)
